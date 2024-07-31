f0168f31959fcc4760999b2a1e32dff6da6a83e9
Luangdl <luam_gdl@hotmail.com>
2024-07-31 19:31:48 -0300

    master

---
+++ primeiro_programa_java/src/cursoJava/Executavel/PrimeiraClasseJava.java
@@ -0,0 +1,8 @@
+package cursoJava.Executavel;
+
+public class PrimeiraClasseJava {
+
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro Programa em Java");
+    }
+}
