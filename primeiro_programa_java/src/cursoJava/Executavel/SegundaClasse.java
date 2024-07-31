4ae3667e0a57551a2f7263d1663d3715d866d2da
Luan Gomes <73196045+Luangdl@users.noreply.github.com>
2024-07-31 20:03:19 -0300

    Update SegundaClasse.java

--- primeiro_programa_java/src/cursoJava/Executavel/SegundaClasse.java
+++ primeiro_programa_java/src/cursoJava/Executavel/SegundaClasse.java
@@ -3,5 +3,6 @@ package cursoJava.Executavel;
 public class SegundaClasse {
     public static void main(String[] args) {
         System.out.println("Segundo programa");
+        SysSystem.out.println("terceiro programa");
     }
 }
